7e3ee5948a1ab536e5d906fdd094177d1e29e682
joao.piccoli <joao.piccoli@L04M16.academico-img.br>
2016-09-17 16:01:51 -0300

    Inclusão do pacote de Huffman

--- ManipulacaoDeArquivos/src/manipulacaodearquivos/ManipulacaoDeArquivos.java
+++ ManipulacaoDeArquivos/src/manipulacaodearquivos/ManipulacaoDeArquivos.java
@@ -5,6 +5,11 @@
  */
 package manipulacaodearquivos;
 
+import huffman.HuffmanFunctions;
+import static huffman.HuffmanFunctions.buildTree;
+import static huffman.HuffmanFunctions.decode;
+import static huffman.HuffmanFunctions.printCodes;
+import huffman.HuffmanTree;
 import java.io.File;
 import java.io.IOException;
 
@@ -26,11 +31,46 @@ public class ManipulacaoDeArquivos {
         SeparadorDePalavras separar = new SeparadorDePalavras();
         SeparacaoDeSilabas separacaoDeSilabas = new SeparacaoDeSilabas();
 
-        File arquivo = criar.CriarArquivo("C:\\Users\\joao.macedo\\Desktop\\Teste.txt");
+        File arquivo = criar.CriarArquivo("C:\\Users\\joao.piccoli\\Desktop\\Teste.txt");
         //escrever.Escrever(arquivo);
         //ler.Leitura(arquivo);
         separar.Separar(arquivo);
         separacaoDeSilabas.Regras();
+        
+        
+        
+        
+        
+        
+        
+        
+         String test = "paralelepipedo"; // Tamanho do texto = 112 bits 
+         HuffmanFunctions hf = new HuffmanFunctions();
+        // Neste exemplo será considerado que teremos no máximo 256 caracteres diferentes
+        // Passo 1 - Percorre o texto contando os símbolos e montando um vetor de frequências.
+        int[] charFreqs = new int[256];
+        for (char c : test.toCharArray())
+            charFreqs[c]++;
+        
+        // Criar a Árvore dos códigos para a Compactação
+        HuffmanTree tree = buildTree(charFreqs);
+        
+        // Resultados das quantidade e o código da Compactação
+        System.out.println("TABELA DE CÓDIGOS");
+        System.out.println("SÍMBOLO\tQUANTIDADE\tHUFFMAN CÓDIGO");
+        printCodes(tree, new StringBuffer());
+        
+        // Compactar o texto
+        String encode = hf.encode(tree,test);
+        // Mostrar o texto Compactado
+        System.out.println("\nTEXTO COMPACTADO");
+        System.out.println(encode); // Tamanho de 40 bits - Economia de 72 bits
+        
+        // Decodificar o texto
+        System.out.println("\n\nTEXTO DECODIFICADO");
+        System.out.println(decode(tree,encode));
     }
+
+   
 
 }
